cface2b028afdf5be1f5e73e70c95cb48aa057e3
Fabian Neugart <f.neugart@mytaxi.com>
2020-04-21 14:23:11 +0200

    renaming

--- cli/src/main/kotlin/TrackingEvents.kt
+++ cli/src/main/kotlin/TrackingEvents.kt
@@ -4,8 +4,10 @@ object TrackingEvents {
 
     @TrackingParam(tracker = MIXPANEL_TRACKER)
     const val DRIVER_MENU_CLICK_QUEST = "DriverMenu_Click_QuestSection"
+
     @TrackingParam(tracker = MIXPANEL_TRACKER)
     const val DRIVER_MENU_CLICK_QUEST2 = "DriverMenu_Click_QuestSection2"
+
     @TrackingParam(tracker = MIXPANEL_TRACKER)
     const val DRIVER_MENU_CLICK_QUEST3 = "DriverMenu_Click_QuestSection3"
 }
